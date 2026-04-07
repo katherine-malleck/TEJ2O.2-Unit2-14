1e135b7a0e0385150c97a9dd05022d2ac97f30ad
katherine <katherine.malleck@stu.ocsb.ca>
2026-04-07 11:12:39 -0400

    did the code

--- main.ts
+++ main.ts
@@ -1,8 +1,97 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: katherine
+ * Created on: Apr 2026
+ * This program will go clockwise and counterclockwise!!!! :D
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let loopCounter = 0
+
+//pressing a button to make it go clockwise
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    // top row going left to right side
+    loopCounter = 0
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 0)
+        loopCounter = loopCounter + 1
+        basic.pause(200)
+    }
+
+    // next one going top to bottom
+    loopCounter = 1
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, 4)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(200)
+    }
+
+    // bottom row
+    loopCounter = 3
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 4)
+        loopCounter = loopCounter - 1
+        basic.pause(200)
+    }
+
+    // bottom to top
+    loopCounter = 3
+    while (loopCounter >= 1) {
+        sprite.set(LedSpriteProperty.X, 0)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(200)
+    }
+
+    sprite.delete()
+})
+
+// a button 
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    // left column (top → bottom)
+    loopCounter = 0
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, 0)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        basic.pause(200)
+    }
+
+    // bottom row (left → right)
+    loopCounter = 1
+    while (loopCounter <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 4)
+        loopCounter = loopCounter + 1
+        basic.pause(200)
+    }
+
+    // right column (bottom → top)
+    loopCounter = 3
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, 4)
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        basic.pause(200)
+    }
+
+    // top row (right → left)
+    loopCounter = 3
+    while (loopCounter >= 1) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        sprite.set(LedSpriteProperty.Y, 0)
+        loopCounter = loopCounter - 1
+        basic.pause(200)
+    }
+
+    sprite.delete()
+})
